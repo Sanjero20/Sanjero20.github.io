4467333c924d976395c6c82a2ba3a3bb3e74e93a
Sanjero <gelosantos72@gmail.com>
2022-10-03 18:00:48 +0800

    Add animation

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,29 @@
 import './style.css';
 import './mobile.css';
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+const languageIcons = document.querySelectorAll('.lang-icon');
+languageIcons.forEach(icon => {
+  icon.classList.add('hidden');
+});
+
+let delay = 0;
+const delayInterval = 100;
+languageIcons.forEach(icon => {
+  delay += delayInterval;
+  icon.setAttribute('style', `transition-delay: ${delay}ms`);
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach(element => {
+  observer.observe(element);
+});
