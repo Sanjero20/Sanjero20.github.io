221fd3cd909df374389ebc4ee86b4bb500218b63
Sanjero20 <gelosantos72@gmail.com>
2023-05-16 23:45:55 +0800

    migrating to React

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <div className="flex gap-1 text-blue-600">
+        <p>hello</p>
+        <p>world</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
