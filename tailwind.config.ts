54833ae1f74ef3e9b3eb260e4205b328e4e08741
Sanjero20 <gelosantos72@gmail.com>
2023-05-17 23:43:31 +0800

    Add fonts and custom color themes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,18 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        body: '#000000',
+        primary: '#0b38ea',
+        secondary: '#fef9e7',
+        gray: '#dde0f4',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        openSans: ['Open Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
